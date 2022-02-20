6e37ff4e27e80a2039ab6472d4db2998bb104269
Swapnil Singh <40495636+swapnil1518@users.noreply.github.com>
2022-02-20 10:52:16 +0530

    Add files via upload

---
+++ Swapping.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a,b = b,a
+
+# we can also use temp to store the value
+# temp = a
+# a = b
+# b = temp
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
