a31ba9371fbaebc7eea0edd9ddf84f03815d9d55
Swapnil Singh <40495636+swapnil1518@users.noreply.github.com>
2022-02-20 10:36:44 +0530

    Update CountLenthOfUserName.py

--- CountLenthOfUserName.py
+++ CountLenthOfUserName.py
@@ -1,1 +1,6 @@
 print(len (input ("Enter user Name ")))
+# this will not show the name you entered in the output sectiuon. So to do this use below program
+
+name = input("What is your name? ")
+print(name)
+print(len(name))
