0ff3c8c0f21f837503b74ee1307aefa8fda92c78
Swapnil Singh <40495636+swapnil1518@users.noreply.github.com>
2022-02-24 07:53:46 +0530

    Add files via upload
    How __init__() method is used inside a python class

---
+++ InitMethod.py
@@ -0,0 +1,14 @@
+class Student:
+# init is method so using def to define it
+    def __init__(self,name,age,branch):
+     self.name = name
+     self.age = age
+     self.branch = branch
+    def print_student(self):
+        print("Name :", self.name)
+        print("Age :", self.age)
+        print("Branch", self.branch)
+student1 = Student("Swapnil",23,"IT")
+#creating obj
+student1.print_student()
+#calling method
