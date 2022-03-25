eccfc69dc03e1fca24c44b38a0e4499eb8b94af9
Swapnil Singh <40495636+swapnil1518@users.noreply.github.com>
2022-03-25 23:34:04 +0530

    Optum Health Care Interview Question

---
+++ OutputProblemInterviewQuestion.py
@@ -0,0 +1,19 @@
+print("Problem 1 ")
+
+i=1
+while i:
+    print('*')
+    i +=1
+    if i==5:
+        break
+else:
+     print('*')
+
+print("Problem 2 ")
+
+i=0
+while i<5:
+    print('*')
+    i +=1
+else:
+    print('*')
