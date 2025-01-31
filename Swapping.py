aa6c97435622bbc34e9adf6227a8d7b3aeca607f
Swapnil Singh <40495636+swapnil1518@users.noreply.github.com>
2025-01-31 14:55:48 +0530

    Update Swapping.py

--- Swapping.py
+++ Swapping.py
@@ -1,23 +1,12 @@
-# 🚨 Don't change the code below 👇
 a = input("a: ")
 b = input("b: ")
-# 🚨 Don't change the code above 👆
-
-####################################
-#Write your code below this line 👇
 
 a,b = b,a
+print(a,b)
 
-# we can also use temp to store the value
-# temp = a
-# a = b
-# b = temp
-
-#Write your code above this line 👆
-####################################
-
-# 🚨 Don't change the code below 👇
-print("a: " + a)
-print("b: " + b)
+ temp = a
+ a = b
+ b = temp
+print(a,b)
 
 
